ad7be4edae4e6ff831164b98adcced56ee93368a
rithmety <ytoune@users.noreply.github.com>
2021-02-11 10:42:46 +0900

    mod

--- abc185/src/bin/e.rs
+++ abc185/src/bin/e.rs
@@ -1,3 +1,42 @@
+#[proconio::fastout]
 fn main() {
-  unimplemented!();
+  proconio::input! {
+    n: usize,
+    m: usize,
+    a: [u64; n],
+    b: [u64; m],
+  };
+  struct Vals {
+    vals: Vec<Vec<Option<u64>>>,
+  }
+  impl Vals {
+    fn pushv(&mut self, n: usize, m: usize, c: u64) {
+      self.vals[n][m] = Some(if let Some(v) = self.vals[n][m] {
+        v.min(c)
+      } else {
+        c
+      });
+    }
+  }
+  let mut vs = Vals {
+    vals: vec![vec![None; m + 2]; n + 2],
+  };
+  vs.vals[0][0] = Some(0);
+  for tn in 0..=n {
+    for tm in 0..=m {
+      if let Some(tc) = vs.vals[tn][tm] {
+        // 消す
+        vs.pushv(tn, tm + 1, tc + 1);
+        vs.pushv(tn + 1, tm, tc + 1);
+        let eq = tn < n && tm < m && a[tn] == b[tm];
+        // 消さない
+        vs.pushv(tn + 1, tm + 1, tc + if eq { 0 } else { 1 });
+      }
+    }
+  }
+  if let Some(tc) = vs.vals[n][m] {
+    println!("{}", tc);
+  } else {
+    println!("{}", m - n);
+  }
 }
--- abc185/src/bin/f.rs
+++ abc185/src/bin/f.rs
@@ -1,3 +1,78 @@
+#[proconio::fastout]
 fn main() {
-  unimplemented!();
+  proconio::input! {
+    n: usize,
+    q: usize,
+    a: [u64; n],
+    queries: [(usize, usize, u64); q],
+  };
+  struct SegmentTree<I: Copy, Op: Fn(I, I) -> I> {
+    buf: Vec<Option<I>>,
+    n: usize,
+    op: Op,
+  }
+  impl<I: Copy, Op: Fn(I, I) -> I> SegmentTree<I, Op> {
+    fn new(a: &[I], op: Op) -> SegmentTree<I, Op> {
+      let mut n = 1;
+      while n < a.len() {
+        n *= 2;
+      }
+      let buf = vec![None; n * 2];
+      let mut st = SegmentTree { buf, n, op };
+      for (i, &v) in a.iter().enumerate() {
+        st.modify(i, v);
+      }
+      st
+    }
+    #[inline]
+    fn calc(&self, q: Option<I>, w: Option<I>) -> Option<I> {
+      match (q, w) {
+        (Some(v1), Some(v2)) => Some((self.op)(v1, v2)),
+        (Some(v1), None) => Some(v1),
+        (_, v2) => v2,
+      }
+    }
+    fn modify(&mut self, idx: usize, val: I) {
+      let mut idx = idx + self.n - 1;
+      self.buf[idx] = Some(val);
+      while 0 < idx {
+        idx = (idx - 1) / 2;
+        self.buf[idx] = self.calc(self.buf[idx * 2 + 1], self.buf[idx * 2 + 2]);
+      }
+    }
+    fn query(&self, l: usize, r: usize) -> Option<I> {
+      let (mut l, mut r) = if l < r { (l, r) } else { (r, l) };
+      let mut res: Option<I> = None;
+      l += self.n - 1;
+      r += self.n - 1;
+      while l < r {
+        if l & 1 == 0 {
+          res = self.calc(self.buf[l], res);
+        }
+        if r & 1 == 0 {
+          res = self.calc(self.buf[r - 1], res);
+        }
+        l /= 2;
+        r = (r - 1) / 2;
+      }
+      res
+    }
+  }
+  let mut sg = SegmentTree::new(&a, |q, w| q ^ w);
+  let sz = sg.buf.len() / 2 - 1;
+  for (t, x, y) in queries {
+    match t {
+      1 => {
+        let x = x - 1;
+        let v = sg.buf[sz + x].unwrap() ^ y;
+        sg.modify(x, v);
+      }
+      2 => {
+        let x = x - 1;
+        let y = y as usize;
+        println!("{}", sg.query(x, y).unwrap());
+      }
+      _ => panic!("unknown t"),
+    }
+  }
 }
