c33c047d11e3020f97959be6dd172c2fe4fd529a
rithmety <ytoune@users.noreply.github.com>
2021-02-27 23:31:01 +0900

    mod

--- abc193/src/bin/b.rs
+++ abc193/src/bin/b.rs
@@ -3,12 +3,12 @@ fn main() {
     n: usize,
     s: [(u64, u64, u64); n],
   };
-  let mut minv = None;
-  for (a, p, x) in s {
-    if a < x {
-      minv = minv.map(|q| if q < p { q } else { p }).or_else(|| Some(p));
-    }
-  }
+  let minv = s
+    .into_iter()
+    .filter(|(a, _, x)| a < x)
+    .fold(None, |v, (_, p, _)| {
+      v.map(|q| if q < p { q } else { p }).or_else(|| Some(p))
+    });
   if let Some(p) = minv {
     println!("{}", p);
   } else {
