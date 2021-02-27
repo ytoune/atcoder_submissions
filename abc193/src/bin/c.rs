052931ee0aba8957fb51230062f8391f3983214a
rithmety <ytoune@users.noreply.github.com>
2021-02-27 23:50:05 +0900

    mod

--- abc193/src/bin/c.rs
+++ abc193/src/bin/c.rs
@@ -2,20 +2,12 @@ fn main() {
   proconio::input! {
     n: u128,
   };
-  if n < 4 {
-    println!("{}", n);
-    return;
-  }
   use std::collections::HashSet;
   let mut map: HashSet<u128> = HashSet::new();
   let mut ans = 0;
   for i in 2..=n {
     let mut j = i.checked_mul(i);
-    if let Some(j2) = j {
-      if n < j2 {
-        break;
-      }
-    } else {
+    if j.map(|j2| n < j2).unwrap_or(true) {
       break;
     }
     while let Some(j2) = j {
@@ -29,9 +21,5 @@ fn main() {
       j = j2.checked_mul(i);
     }
   }
-  if n >= ans {
-    println!("{}", n - ans);
-  } else {
-    println!("0");
-  }
+  println!("{}", n - ans);
 }
