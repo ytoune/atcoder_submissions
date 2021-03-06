602bc2a959c8d1469945687c4429243e5a5bcaec
rithmety <ytoune@users.noreply.github.com>
2021-03-07 01:59:05 +0900

    mod

--- abc194/src/bin/b.rs
+++ abc194/src/bin/b.rs
@@ -10,11 +10,9 @@ fn main() {
   let a_2 = need_a.iter().filter(|v| a_1.1 != v.1).min().unwrap();
   let b_1 = need_b.iter().min().unwrap();
   let b_2 = need_b.iter().filter(|v| b_1.1 != v.1).min().unwrap();
-  let list = [(a_1, b_1), (a_2, b_1), (a_1, b_2), (a_2, b_2)];
-  let mut min: Option<usize> = None;
-  for &(a, b) in list.iter() {
-    let tm = if a.1 == b.1 { a.0 + b.0 } else { a.0.max(b.0) };
-    min = min.map(|t| t.min(tm)).or_else(|| Some(tm));
-  }
+  let min = [(a_1, b_1), (a_2, b_1), (a_1, b_2), (a_2, b_2)]
+    .iter()
+    .map(|&(a, b)| if a.1 == b.1 { a.0 + b.0 } else { a.0.max(b.0) })
+    .min();
   println!("{}", min.unwrap());
 }
