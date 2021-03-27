7a4d5d7525241ac1d814414bd63e5bd7978e16a7
rithmety <ytoune@users.noreply.github.com>
2021-03-28 00:37:16 +0900

    mod

--- abc197/src/bin/e.rs
+++ abc197/src/bin/e.rs
@@ -3,39 +3,36 @@ fn main() {
     n: usize,
     balls: [(i64, usize); n],
   };
-  use std::cmp::Reverse;
-  use std::collections::BinaryHeap;
   let balls = {
+    use itertools::Itertools;
     let mut balls: Vec<_> = balls.into_iter().map(|(pos, color)| (color, pos)).collect();
     balls.sort();
+    let balls: Vec<_> = balls
+      .into_iter()
+      .group_by(|q| q.0)
+      .into_iter()
+      .map(|(color, items)| {
+        let mut min: Option<i64> = None;
+        let mut max: Option<i64> = None;
+        for (_, p) in items {
+          min = min.map(|v| v.min(p)).or_else(|| Some(p));
+          max = max.map(|v| v.max(p)).or_else(|| Some(p));
+        }
+        (color, min.unwrap(), max.unwrap())
+      })
+      .collect();
     balls
   };
-  let mut que = BinaryHeap::new();
-  que.push((Reverse(0i64), Reverse(0i64), n, vec![false; n]));
-  while let Some((Reverse(time), Reverse(pos), count, done)) = que.pop() {
-    if 0 == count {
-      println!("{}", time + pos.abs());
-      return;
-    }
-    let mut prev: Option<usize> = None;
-    for (i, b) in balls.iter().enumerate() {
-      if done[i] {
-        continue;
-      }
-      if let Some(p) = prev {
-        if p != b.0 {
-          break;
-        }
-      } else {
-        prev = Some(b.0);
-      }
-      let done: Vec<_> = done.iter().enumerate().map(|(j, d)| i == j || *d).collect();
-      que.push((
-        Reverse(time + (b.1 - pos).abs()),
-        Reverse(b.1),
-        count - 1,
-        done,
-      ));
+  let mut dp = vec![vec![(0, 0); 2]; balls.len() + 1];
+  for (i, b) in balls.iter().enumerate() {
+    for (j, &(b1, b2)) in [(b.1, b.2), (b.2, b.1)].iter().enumerate() {
+      dp[i + 1][j] = dp[i]
+        .iter()
+        .map(|d| (d.0 + (d.1 - b1).abs() + (b1 - b2).abs(), b2))
+        .min()
+        .unwrap();
     }
   }
+  let min = dp.last().unwrap().iter().min().unwrap();
+  println!("{}", min.0 + min.1.abs());
 }
