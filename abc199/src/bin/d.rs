6d09ae0a24d9f71867ba444882d7eec383b198a6
rithmety <ytoune@users.noreply.github.com>
2021-04-25 06:33:29 +0900

    mod

--- abc199/src/bin/d.rs
+++ abc199/src/bin/d.rs
@@ -9,100 +9,65 @@ fn main() {
 }
 
 fn solve(n: usize, edges: Vec<(usize, usize)>) -> u128 {
-  #[derive(Clone, Debug)]
-  struct DotColor {
-    pub color: Option<usize>, // 0, 1, 2
-    pub fixed: bool,
-    pub not: [bool; 3],
-  }
-  impl DotColor {
-    fn new() -> DotColor {
-      DotColor {
-        color: None,
-        fixed: false,
-        not: [false; 3],
-      }
-    }
-    fn count_possibility(&self) -> (u128, Option<usize>) {
-      let mut a = 0;
-      let mut r: Option<usize> = None;
-      for ci in 0..3 {
-        if !self.not[ci] {
-          a += 1;
-          if r.is_none() {
-            r = Some(ci);
-          }
-        }
-      }
-      (a, r)
-    }
-  }
-  impl Default for DotColor {
-    fn default() -> DotColor {
-      DotColor::new()
-    }
+  use petgraph::unionfind::UnionFind;
+  let mut uf = UnionFind::<usize>::new(n);
+  for (a, b) in edges.iter().copied() {
+    uf.union(a, b);
   }
 
-  // use petgraph::graph::UnGraph;
-  // let mut graph = UnGraph::from_edges(edges.iter());
-  let edges = {
-    let mut e: Vec<Vec<usize>> = vec![vec![]; n];
-    for (a, b) in edges {
-      e[a].push(b);
-      e[b].push(a);
-    }
-    e
-  };
+  use petgraph::graph::UnGraph;
+  use petgraph::{Graph, Undirected};
+  type Gr = Graph<(), (), Undirected, usize>;
+  let gr: Gr = UnGraph::<(), (), usize>::from_edges(&edges);
+
   use std::collections::*;
-  let mut done: HashSet<(usize, usize)> = HashSet::new();
-  let mut ans: u128 = 1;
-  let mut colors = vec![DotColor::new(); n];
-  let mut que: VecDeque<(usize, Option<(usize, usize)>)> = VecDeque::new();
-  fn edge(a: usize, b: usize) -> (usize, usize) {
-    if a <= b {
-      (a, b)
-    } else {
-      (b, a)
-    }
+  let mut grs = HashMap::<usize, (usize, usize)>::new();
+  for n in 0..n {
+    let l = uf.find_mut(n);
+    grs
+      .entry(l)
+      .and_modify(|u| {
+        u.1 += 1;
+      })
+      .or_insert((n, 1));
   }
-  'main: loop {
-    if let Some((i, prev)) = que.pop_back() {
-      let c = &mut colors[i];
-      if c.fixed {
-        continue 'main;
-      }
-      if let Some((p, ci)) = prev {
-        let e = edge(i, p);
-        if done.contains(&e) {
-          continue 'main;
-        }
-        c.not[ci] = true;
-        done.insert(e);
-      } else {
-        c.fixed = true;
-      }
-      let (_a, ci) = c.count_possibility();
-      if let Some(ci) = ci {
-        c.color = Some(ci);
-        for j in edges[i].iter().copied() {
-          que.push_back((j, Some((i, ci))));
-        }
-        continue 'main;
-      }
-      return 0;
-    } else {
-      for (i, c) in colors.iter().enumerate() {
-        if c.color.is_none() {
-          que.push_back((i, None));
-          continue 'main;
-        }
-      }
-      println!("{:?}", colors);
-      for c in colors.iter() {
-        let (a, _) = c.count_possibility();
-        ans *= a;
-      }
-      return ans;
+
+  let mut ans = 1;
+
+  for (_, (i, size)) in grs {
+    if size == 1 {
+      ans *= 3;
+      continue;
     }
+    use petgraph::visit::Dfs;
+    let mut nodes = vec![];
+    let mut dfs = Dfs::new(&gr, i.into());
+    while let Some(n) = dfs.next(&gr) {
+      nodes.push(n.index());
+    }
+    fn bfs(gr: &Gr, cur: usize, nodes: &[usize], colors: &mut [i32]) -> u128 {
+      if cur == nodes.len() {
+        return 1;
+      }
+      let mut res = 0;
+      let i = nodes[cur];
+      'color: for c in 1..=3 {
+        for j in gr.neighbors(i.into()) {
+          let ji = j.index();
+          if colors[ji] != 0 && colors[ji] == c {
+            continue 'color;
+          }
+        }
+        colors[i] = c;
+        res += bfs(gr, cur + 1, nodes, colors);
+        colors[i] = 0;
+      }
+      res
+    }
+    let mut colors = vec![0; n];
+    colors[nodes[0]] = 1;
+    ans *= 3 * bfs(&gr, 1, &nodes, &mut colors);
   }
+
+  ans
 }
