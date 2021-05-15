4d13f422a2cc75898811f191bf40f8d88382c003
rithmety <ytoune@users.noreply.github.com>
2021-05-15 23:09:57 +0900

    mod

--- abc201/src/bin/c.rs
+++ abc201/src/bin/c.rs
@@ -2,30 +2,23 @@ fn main() {
   use proconio::marker::Chars;
   proconio::input! { s: Chars };
   let mut ans = 0;
-  for n1 in 0..10 {
-    for n2 in 0..10 {
-      for n3 in 0..10 {
-        'main: for n4 in 0..10 {
-          for (n, c) in s.iter().enumerate() {
-            match *c {
-              'o' => {
-                if n != n1 && n != n2 && n != n3 && n != n4 {
-                  continue 'main;
-                }
-              }
-              'x' => {
-                if n == n1 || n == n2 || n == n3 || n == n4 {
-                  continue 'main;
-                }
-              }
-              '?' => {}
-              _ => unreachable!(),
-            }
-          }
-          ans += 1;
-        }
-      }
+  use itertools::Itertools;
+  for ns in (0..10)
+    .cartesian_product(0..10)
+    .cartesian_product(0..10)
+    .cartesian_product(0..10)
+    .map(|(((n1, n2), n3), n4)| [n1, n2, n3, n4])
+  {
+    let has_bad = s.iter().enumerate().any(|(n, c)| match *c {
+      'o' => ns.iter().all(|t| n != *t),
+      'x' => ns.iter().any(|t| n == *t),
+      '?' => false,
+      _ => unreachable!(),
+    });
+    if has_bad {
+      continue;
     }
+    ans += 1;
   }
   println!("{}", ans);
 }
