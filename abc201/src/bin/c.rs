d61bb90df206c2e32ce9aa746a4ff47f7bdbbc21
rithmety <ytoune@users.noreply.github.com>
2021-05-15 23:29:22 +0900

    mod

--- abc201/src/bin/c.rs
+++ abc201/src/bin/c.rs
@@ -2,13 +2,8 @@ fn main() {
   use proconio::marker::Chars;
   proconio::input! { s: Chars };
   let mut ans = 0;
-  use itertools::Itertools;
-  for ns in (0..10)
-    .cartesian_product(0..10)
-    .cartesian_product(0..10)
-    .cartesian_product(0..10)
-    .map(|(((n1, n2), n3), n4)| [n1, n2, n3, n4])
-  {
+  use itertools::iproduct;
+  for ns in iproduct!(0..10, 0..10, 0..10, 0..10).map(|(n1, n2, n3, n4)| [n1, n2, n3, n4]) {
     let has_bad = s.iter().enumerate().any(|(n, c)| match *c {
       'o' => ns.iter().all(|t| n != *t),
       'x' => ns.iter().any(|t| n == *t),
