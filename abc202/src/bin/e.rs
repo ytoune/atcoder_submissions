4fe06f42e162668c4e3c927ee099502084f99567
rithmety <ytoune@users.noreply.github.com>
2021-05-22 23:56:56 +0900

    mod

--- abc202/src/bin/e.rs
+++ abc202/src/bin/e.rs
@@ -1,46 +1,60 @@
+#[proconio::fastout]
 fn main() {
   use proconio::marker::Usize1;
   proconio::input! {
     n: usize,
-    p: [Usize1; n-1],
+    p: [Usize1; n - 1],
     queries: [(Usize1, usize)],
   }
-  let childrens = {
+  let parent = |idx: usize| p[idx - 1];
+  let childrens: Vec<Vec<usize>> = {
     let mut childrens = vec![vec![]; n];
-    for (i, &pi) in p.iter().enumerate() {
-      let i = i + 1;
-      childrens[pi].push(i);
+    for i in 1..n {
+      childrens[parent(i)].push(i);
     }
     childrens
   };
-  let mut descendants = vec![vec![]; n];
-  for (i, &pi) in p.iter().enumerate() {
-    let i = i + 1;
-    let mut pi = pi;
-    loop {
-      descendants[pi].push(i);
-      if pi == 0 {
-        break;
+  let descendants: Vec<Vec<usize>> = {
+    let mut descendants = vec![vec![]; n];
+    descendants[0].push(0);
+    for i in 1..n {
+      descendants[i].push(i);
+      let mut pi = parent(i);
+      loop {
+        descendants[pi].push(i);
+        if pi == 0 {
+          break;
+        }
+        pi = parent(pi);
       }
-      pi = p[pi - 1];
     }
-  }
-  let mut heights = vec![0; n];
-  let mut que = std::collections::VecDeque::new();
-  que.push_front((0, 1));
-  while let Some((i, h)) = que.pop_back() {
-    heights[i] = h;
-    for &j in childrens[i].iter() {
-      que.push_front((j, h + 1));
+    descendants
+  };
+  use std::collections::*;
+  let heights: Vec<usize> = {
+    let mut heights = vec![0; n];
+    let mut que = VecDeque::new();
+    que.push_front((0, 0));
+    while let Some((i, h)) = que.pop_back() {
+      heights[i] = h;
+      for &j in childrens[i].iter() {
+        que.push_front((j, h + 1));
+      }
     }
-  }
+    heights
+  };
+  let counted: Vec<Vec<u64>> = descendants
+    .iter()
+    .map(|des| {
+      let mut tmp = vec![0; n];
+      for &u in des.iter() {
+        tmp[heights[u]] += 1;
+      }
+      tmp
+    })
+    .collect();
   for (c, d) in queries.into_iter() {
-    let mut ans = 0;
-    for &u in descendants[c].iter() {
-      if d == heights[u] {
-        ans += 1;
-      }
-    }
+    let ans = counted[c][d];
     println!("{}", ans);
   }
 }
