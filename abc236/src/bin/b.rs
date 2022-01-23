0f9a1d91a50668835274be138a6f0f30eaf9901c
rithmety <ytoune@users.noreply.github.com>
2022-01-24 00:02:15 +0900

    mod

--- abc236/src/bin/b.rs
+++ abc236/src/bin/b.rs
@@ -6,8 +6,9 @@ fn main() {
   use std::collections::BTreeMap;
   let mut nums: BTreeMap<u64, u64> = BTreeMap::new();
   for num in a.iter().cloned() {
-    let v = nums.get(&num).unwrap_or(&0);
-    nums.insert(num, *v + 1);
+    nums.entry(num)
+      .and_modify(|v| { *v += 1 })
+      .or_insert(1);
   }
   for (&num, &count) in nums.iter() {
     if 3 == count {
